d0011026415d786f7f94420d260f19b2c9b2f513
dato <Jincharadze.Davit@kiu.edu.ge>
2024-01-03 00:43:20 +0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,26 +1,44 @@
 public class Customer {
-    String name;
-    int money;
+     String name;
+     int money;
+     Cart<MarketItem> cart;
     public Customer(String name,int money) throws IllegalArgumentException{
         if( name == null || money< 0){
             throw new IllegalArgumentException("This customer either does not exist or does not have any money");
         }
         this.name = name;
         this.money = money;
+        cart = new Cart();
+    }
+
+    public Cart<MarketItem> getCart() {
+        return cart;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+    public void setMoney(int money){
+        this.money = money;
     }
 
     public void addItemToCart(MarketItem item, int n) throws IllegalArgumentException{
-        if(item == null){
+        if(item == null || n == 0){
             throw new IllegalArgumentException("this item cant be added to the cart");
         }
-        addItemToCart(item,n);
+        cart.addItem(item,n);
+
     }
 
     public boolean removeItemFromCart(MarketItem item, int n) throws IllegalArgumentException{
         if(item == null){
             throw new IllegalArgumentException("this item does not exist, therefore can not be removed from the cart");
         }
-        removeItemFromCart(item,n);
+        cart.removeItem(item,n);
         return true;
     }
     public boolean buyItemsFromCart(){
@@ -28,8 +46,7 @@ public class Customer {
      if(money - cart.calculatePrice() <= 0){
          return false;
      }
-     money = money - cart.calculatePrice();
-     cart.clear;
+     cart.checkOut(money);
      return true;
     }
 }
